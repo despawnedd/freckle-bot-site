a5142f9c7f2478129d08b884db852fde0f317896
despawnedd <despawnedd@gmail.com>
2025-07-26 20:10:09 +0200

    fix?

--- src/redirects.js
+++ src/redirects.js
@@ -1,5 +1,6 @@
 // https://supun.io/sveltekit-static-redirects
 
 export const APP_REDIRECTS = {
-    '/docs': '/docs/what-is-freckle'
-}
+    "/docs": "/docs/what-is-freckle",
+    "/freckle-bot-site/docs": "/freckle-bot-site/docs/what-is-freckle"
+};
