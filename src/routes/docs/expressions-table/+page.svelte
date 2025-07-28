fc935143b7352e5a83db1ac7c171691956d84ddb
despawnedd <despawnedd@gmail.com>
2025-07-28 18:53:49 +0200

    Move characters to separate ts file

--- src/routes/docs/expressions-table/+page.svelte
+++ src/routes/docs/expressions-table/+page.svelte
@@ -5,66 +5,8 @@
     export const title = "Textbox character expressions table";
 
     import { base } from "$app/paths";
+    import { chars, type Whatever } from "./characters";
 
-    interface Whatever {
-        id: string;
-        name: string;
-        href: string;
-        hrefTitle: string;
-        length: number;
-    }
-
-    const chars: Whatever[] = [
-        {
-            id: "noelle",
-            name: "Noelle",
-            href: "https://www.spriters-resource.com/pc_computer/deltarune/sheet/110612/",
-            hrefTitle: "PC / Computer - Deltarune - Noelle - The Spriters Resource",
-            length: 51
-        },
-        {
-            id: "ralsei",
-            name: "Ralsei",
-            href: "https://www.spriters-resource.com/pc_computer/deltarune/sheet/110469/",
-            hrefTitle: "PC / Computer - Deltarune - Ralsei - The Spriters Resource",
-            length: 51
-        },
-        {
-            id: "susie",
-            name: "Susie",
-            href: "https://www.spriters-resource.com/pc_computer/deltarune/sheet/110471/",
-            hrefTitle: "PC / Computer - Deltarune - Susie (Chapter 1&2) - The Spriters Resource",
-            length: 51
-        },
-        {
-            id: "kris",
-            name: "Kris",
-            href: "https://x.com/EllisBros/status/1938375472850812938",
-            hrefTitle: '🩸EllisBros🩸 on X: "kris portrait resprite or something https://t.co/WOw4ggnb7q" / X',
-            length: 10
-        },
-        {
-            id: "sans",
-            name: "Sans",
-            href: "https://www.spriters-resource.com/pc_computer/undertale/sheet/76011/",
-            hrefTitle: "PC / Computer - Undertale - Sans - The Spriters Resource" ,
-            length: 7
-        },
-        {
-            id: "sansboss",
-            name: "Sans (boss)",
-            href: "https://www.spriters-resource.com/pc_computer/undertale/sheet/76011/",
-            hrefTitle: "PC / Computer - Undertale - Sans - The Spriters Resource" ,
-            length: 22
-        },
-        {
-            id: "papyrus",
-            name: "Papyrus",
-            href: "https://www.spriters-resource.com/pc_computer/undertale/sheet/76014/",
-            hrefTitle: "PC / Computer - Undertale - Papyrus - The Spriters Resource" ,
-            length: 19
-        }
-    ];
 </script>
 
 <svelte:head>
